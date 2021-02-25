c51ac21d8b5446e62874605d5931337015d4f807
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-02-25 00:01:08 -0800

    comment

--- snowpack.config.js
+++ snowpack.config.js
@@ -13,9 +13,11 @@ module.exports = {
     /* Use built-in esbuild bundling for build */
     bundle: true,
     minify: true,
+    target: "es2020",
     treeshake: true,
     splitting: true,
-    target: "es2020",
+    // Additional opts:
+    // https://www.snowpack.dev/guides/optimize-and-bundle#option-1%3A-built-in-optimizations
   },
   packageOptions: {
     /* ... */
